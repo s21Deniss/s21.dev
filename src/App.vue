46b3dd8fc027b50c0445f1d13ba429eb60fddf71
s21deniss <smallzloj@gmail.com>
2019-12-28 17:27:09 +0200

    remove

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view/>
-  </main>
-</template>
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/s21Deniss.github.io/' : '/',
-}
